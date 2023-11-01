bb5e9132c33c338d929ab9c4eb249f1c2a83fedb
ProstoEvgenii <98259817+ProstoEvgenii@users.noreply.github.com>
2023-11-01 15:46:59 +0300

    .

--- frontends/settings/display/Main.tsx
+++ frontends/settings/display/Main.tsx
@@ -4,81 +4,87 @@ export default function () {
     return (
         <div class="home home_container">
             <div>
-                <input
-                    value={this.Static.record.template ? this.Static.settingsForm.template = this.Static.record.template : ""}
-                    placeholder="Название шаблона письма"
-                    ref="inputTemplateName"
-                    type="text"
-                    oninput={(e) => {
-                        this.Static.settingsForm.template = e.target.value
-                    }}
-                />
-                <br />
-                <input
-                    value={this.Static.record.emailLogin ? this.Static.settingsForm.emailLogin = this.Static.record.emailLogin : ""}
-                    placeholder="E-mail"
-                    ref="inputEmail"
-                    type="text"
-                    oninput={(e) => {
-                        this.Static.settingsForm.emailLogin = e.target.value
-                    }}
-                />
-                <br />
-                <input
-                    placeholder="Пароль почты"
-                    ref="inputEmailPass"
-                    type="text"
-                    oninput={(e) => {
-                        this.Static.settingsForm.emailPass = e.target.value
-                    }}
-                />
-                <br />
-                <input
-                    placeholder="smtp"
-                    value={this.Static.record.smtp ? this.Static.settingsForm.smtp = this.Static.record.smtp : ""}
-                    ref="inputSmtp"
-                    type="text"
-                    oninput={(e) => {
-                        this.Static.settingsForm.smtp = e.target.value
-                    }}
-                />
-                <br />
-                <input
-                    placeholder="Порт"
-                    value={this.Static.record.port ? this.Static.settingsForm.port = this.Static.record.port : ""}
-                    ref="inputPort"
-                    type="text"
-                    oninput={(e) => {
-                        this.Static.settingsForm.port = e.target.value
-                    }}
-                />
-                <br />
-                <button
-                    onclick={() => {
-                        this.fn("validateForm")
-                    }}
-                >
-                    Сохранить настройки
-                </button>
-                <div></div>
-                <br />
-                <div>
-                    {
-                        this.Static.postResponse.documentsInserted == 0 ? <div></div> :
-                            <div>
-                                Настройки cохранены
-                            </div>
-                    }
+                <div class="form">
+                    <input
+                        value={this.Static.record.template ? this.Static.settingsForm.template = this.Static.record.template : ""}
+                        placeholder="Название шаблона письма"
+                        ref="inputTemplateName"
+                        class="form__input"
+                        type="text"
+                        oninput={(e) => {
+                            this.Static.settingsForm.template = e.target.value
+                        }}
+                    />
+                    <br />
+                    <input
+                        value={this.Static.record.emailLogin ? this.Static.settingsForm.emailLogin = this.Static.record.emailLogin : ""}
+                        placeholder="E-mail"
+                        ref="inputEmail"
+                        class="form__input"
+                        type="text"
+                        oninput={(e) => {
+                            this.Static.settingsForm.emailLogin = e.target.value
+                        }}
+                    />
+                    <br />
+                    <input
+                        placeholder="Пароль почты"
+                        ref="inputEmailPass"
+                        class="form__input"
+                        type="text"
+                        oninput={(e) => {
+                            this.Static.settingsForm.emailPass = e.target.value
+                        }}
+                    />
+                    <br />
+                    <input
+                        placeholder="smtp"
+                        value={this.Static.record.smtp ? this.Static.settingsForm.smtp = this.Static.record.smtp : ""}
+                        ref="inputSmtp"
+                        class="form__input"
+                        type="text"
+                        oninput={(e) => {
+                            this.Static.settingsForm.smtp = e.target.value
+                        }}
+                    />
+                    <br />
+                    <input
+                        placeholder="Порт"
+                        value={this.Static.record.port ? this.Static.settingsForm.port = this.Static.record.port : ""}
+                        ref="inputPort"
+                        class="form__input"
+                        type="text"
+                        oninput={(e) => {
+                            this.Static.settingsForm.port = e.target.value
+                        }}
+                    />
+                    <br />
+                    <button
+                        onclick={() => {
+                            this.fn("validateForm")
+                        }}
+                    >
+                        Сохранить настройки
+                    </button>
+                    <div></div>
+                    <br />
+                    <div>
+                        {
+                            this.Static.postResponse.documentsInserted == 0 ? <div></div> :
+                                <div>
+                                    Настройки cохранены
+                                </div>
+                        }
+                    </div>
+                    <div>
+                        {
+                            this.Static.postResponse.documentsModified == 0 ? <div></div> :
+                                <div>
+                                    Настройки обновлены
+                                </div>
+                        }
+                    </div>
                 </div>
-                <div>
-                    {
-                        this.Static.postResponse.documentsModified == 0 ? <div></div> :
-                            <div>
-                                Настройки обновлены
-                            </div>
-                    }
-                </div>
-
             </div>
         </div>
 
