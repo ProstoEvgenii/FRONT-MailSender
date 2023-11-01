4bea814560860ac04d868eade6b757454490530f
ProstoEvgenii <98259817+ProstoEvgenii@users.noreply.github.com>
2023-11-01 16:52:48 +0300

    .

--- frontends/settings/display/Main.tsx
+++ frontends/settings/display/Main.tsx
@@ -2,64 +2,66 @@ import { Cemjsx } from "cemjs-all"
 export default function () {
     // console.log('=c937e1=',this.Static.postResponse)
     return (
-        <div class="home home_container">
-            <div>
-                <div class="form">
+        <div class="settings">
+            <div class="settings__fields__container" >
+    
+                <div class="settings__fields">
                     <input
                         value={this.Static.record.template ? this.Static.settingsForm.template = this.Static.record.template : ""}
                         placeholder="Название шаблона письма"
                         ref="inputTemplateName"
-                        class="form__input"
+                        class="field__input"
                         type="text"
                         oninput={(e) => {
                             this.Static.settingsForm.template = e.target.value
                         }}
                     />
-                    <br />
+
                     <input
                         value={this.Static.record.emailLogin ? this.Static.settingsForm.emailLogin = this.Static.record.emailLogin : ""}
                         placeholder="E-mail"
                         ref="inputEmail"
-                        class="form__input"
+                        class="field__input"
                         type="text"
                         oninput={(e) => {
                             this.Static.settingsForm.emailLogin = e.target.value
                         }}
                     />
-                    <br />
+
                     <input
                         placeholder="Пароль почты"
                         ref="inputEmailPass"
-                        class="form__input"
+                        class="field__input"
                         type="text"
                         oninput={(e) => {
                             this.Static.settingsForm.emailPass = e.target.value
                         }}
                     />
-                    <br />
+
                     <input
                         placeholder="smtp"
                         value={this.Static.record.smtp ? this.Static.settingsForm.smtp = this.Static.record.smtp : ""}
                         ref="inputSmtp"
-                        class="form__input"
+                        class="field__input"
                         type="text"
                         oninput={(e) => {
                             this.Static.settingsForm.smtp = e.target.value
                         }}
                     />
-                    <br />
+
                     <input
                         placeholder="Порт"
                         value={this.Static.record.port ? this.Static.settingsForm.port = this.Static.record.port : ""}
                         ref="inputPort"
-                        class="form__input"
+                        class="field__input"
                         type="text"
                         oninput={(e) => {
                             this.Static.settingsForm.port = e.target.value
                         }}
                     />
-                    <br />
+
                     <button
+                        class="btn btn__primary"
                         onclick={() => {
                             this.fn("validateForm")
                         }}
@@ -67,7 +69,7 @@ export default function () {
                         Сохранить настройки
                     </button>
                     <div></div>
-                    <br />
+
                     <div>
                         {
                             this.Static.postResponse.documentsInserted == 0 ? <div></div> :
