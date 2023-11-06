fe4819bf86e2591ae2a21b188348dbe865d196b2
ProstoEvgenii <98259817+ProstoEvgenii@users.noreply.github.com>
2023-11-06 17:30:01 +0300

    Revert "."
    This reverts commit be3e99d1346c89fe2fd7638b6c5d37ce510184f8.

--- frontends/settings/display/Main.tsx
+++ frontends/settings/display/Main.tsx
@@ -1,32 +1,31 @@
 import { Cemjsx } from "cemjs-all"
 export default function () {
-    // console.log('=c937e1=',this.Static.record.events_records?.IsDaily)
+    // console.log('=c937e1=',this.Static.postResponse)
     return (
-        <div class="settings">
+        <div class="settings info section">
 
             <div class="settings__fields__container" >
                 <p class="settingsTitle">Настройка почты</p>
                 <div class="settings__fields">
                     <input
-                        value={this.Static.record ? this.Static.settingsFormEmail.template = this.Static.record.settings?.template : ""}
+                        value={this.Static.record.template ? this.Static.settingsForm.template = this.Static.record.template : ""}
                         placeholder="Название шаблона письма"
                         ref="inputTemplateName"
                         class="field__input"
                         type="text"
                         oninput={(e) => {
-                            this.Static.settingsFormEmail.template = e.target.value
+                            this.Static.settingsForm.template = e.target.value
                         }}
                     />
 
                     <input
-                        value={this.Static.record ? this.Static.settingsFormEmail.emailLogin = this.Static.record.settings?.emailLogin : ""}
+                        value={this.Static.record.emailLogin ? this.Static.settingsForm.emailLogin = this.Static.record.emailLogin : ""}
                         placeholder="E-mail"
-
+                        ref="inputEmail"
                         class="field__input"
                         type="text"
-                        ref="inputEmail"
                         oninput={(e) => {
-                            this.Static.settingsFormEmail.emailLogin = e.target.value
+                            this.Static.settingsForm.emailLogin = e.target.value
                         }}
                     />
 
@@ -36,36 +35,36 @@ export default function () {
                         class="field__input"
                         type="text"
                         oninput={(e) => {
-                            this.Static.settingsFormEmail.emailPass = e.target.value
+                            this.Static.settingsForm.emailPass = e.target.value
                         }}
                     />
 
                     <input
                         placeholder="smtp"
-                        value={this.Static.record ? this.Static.settingsFormEmail.smtp = this.Static.record.settings?.smtp : ""}
+                        value={this.Static.record.smtp ? this.Static.settingsForm.smtp = this.Static.record.smtp : ""}
                         ref="inputSmtp"
                         class="field__input"
                         type="text"
                         oninput={(e) => {
-                            this.Static.settingsFormEmail.smtp = e.target.value
+                            this.Static.settingsForm.smtp = e.target.value
                         }}
                     />
 
                     <input
                         placeholder="Порт"
-                        value={this.Static.record ? this.Static.settingsFormEmail.port = this.Static.record.settings?.port : ""}
+                        value={this.Static.record.port ? this.Static.settingsForm.port = this.Static.record.port : ""}
                         ref="inputPort"
                         class="field__input"
                         type="text"
                         oninput={(e) => {
-                            this.Static.settingsFormEmail.port = e.target.value
+                            this.Static.settingsForm.port = e.target.value
                         }}
                     />
 
                     <button
                         class="butn btn__primary"
                         onclick={() => {
-                            this.fn("validateSettingsForm")
+                            this.fn("validateForm")
                         }}
                     >
                         Сохранить настройки
@@ -90,142 +89,6 @@ export default function () {
                     </div>
                 </div>
             </div>
-            <table class="history_table">
-                <thead >
-                    <tr class="history_table-head">
-                        <th class="history_table__cell">№</th>
-                        <th class="history_table__cell">Название</th>
-                        <th class="history_table__cell">Шаблон</th>
-                        <th class="history_table__cell">Время отравки</th>
-                        <th class="history_table__cell">Режим</th>
-                        {/* <th class="history_table__cell">Дата</th>
-                        <th class="history_table__cell">вкл/выкл</th> */}
-                    </tr>
-                </thead>
-                <tbody class="history_table-body">
-                    {/* <tr>
-                        <button
-                            class="butn btn__primary"
-                            onclick={() => {
-
-                                // console.log('=c5880a=', this.Ref.sendMode.value)
-                                this.Static.settingsFormNewEvent.isDaily=this.Ref.sendMode.value
-                                this.fn("validateEventForm")
-                            }}
-                        >
-                            Сохранить настройки
-                        </button>
-                        <td class="history_table__cell">
-                            <input
-                                type="text"
-                                ref="inputEventName"
-                                oninput={(e) => {
-                                    this.Static.settingsFormNewEvent.name = e.target.value
-                                }}
-
-                            />
-                        </td>
-                        <td class="history_table__cell"
-                        >
-                            <input
-                                type="text"
-                                oninput={(e) => {
-                                    this.Static.settingsFormNewEvent.template = e.target.value
-                                }}
-                            />
-                        </td>
-                        <td class="history_table__cell"
-                        >
-                            <input
-                                type="number"
-                                min="1"
-                                max="24"
-                                oninput={(e) => {
-                                    this.Static.settingsFormNewEvent.sendAt = e.target.value
-                                }}
-                            />
-                        </td>
-                        <td class="history_table__cell"
-                        >
-                            <select
-                                name=""
-                                ref="sendMode"
-                                id=""
-                            >
-                                Режим отправки
-                                <option value="daily">Ежедневно</option>
-                                <option value="onDate">В указанную дату</option>
-                            </select>
-                        </td>
-                        <td class="history_table__cell">
-                            <input
-                                type="text"
-                                placeholder="День (например, 28)"
-                                oninput={(e) => {
-                                    this.Static.settingsFormNewEvent.date = e.target.value
-                                }}
-                            />
-                            <input
-                                type="text"
-                                placeholder="Месяц (например, 05)"
-                                oninput={(e) => {
-                                    this.Static.settingsFormNewEvent.month = e.target.value
-                                }}
-                            />
-                        </td>
-                        <td class="history_table__cell">
-                            {/* <input
-                                type="text"
-                                oninput={(e) => {
-                                    this.Static.settingsFormNewEvent.active = e.target.value
-                                }}
-                            /> */}
-                        {/* </td>
-                    </tr> */}
-                    {
-
-                        this.Static.record.events_records?.map((item, index) => {
-                            return (
-
-                                <tr>
-                                    <td class="history_table__cell">{index + 1}</td>
-                                    <td class="history_table__cell">{item.name}</td>
-                                    <td class="history_table__cell">{item.template}</td>
-                                    <td class="history_table__cell">{item.sendAt}</td>
-                                    <td
-                                        class="history_table__cell" value="GHb"
-                                    >
-                                        Ежедневно
-                                        {/* <div>{item.isDaily == "daily"? }</div> */}
-                                        
-                                         {/* <div class="switch">
-                                            <div class="switch__1">
-                                                <input
-                                                    id="switch-1"
-                                                    type="checkbox"
-                                                    ref="switch"
-                                                    // checked={false}
-                                                    onchange={(e) => {
-
-                                                        // console.log('=f0eae2=', e.target.checked)
-                                                        // e.target.checked = item.IsDaily
-                                                    }}
-
-                                                />
-                                                <label for="switch-1"></label>
-                                            </div>
-                                        </div>  */}
-                                    </td>
-                                    {/* <td class="history_table__cell">{item.date}</td>
-                                    <td class="history_table__cell">{item.active}</td> */}
-                                </tr>
-                            )
-                        })
-                    }
-
-                </tbody>
-            </table>
-
 
 
         </div>
--- frontends/settings/loader.ts
+++ frontends/settings/loader.ts
@@ -1,28 +1,13 @@
 export const loader = function () {
     this.fn("printHello")
     this.fn("makeRequest")
-
-
-    this.Static.settingsFormEmail = {
-
+    this.Static.settingsForm = {
         template: "",
         emailLogin: "",
         emailPass: "",
         smtp: "",
         port: 0
-
     }
-    this.Static.settingsFormNewEvent = {
-        name: "",
-        template: "",
-        sendAt: 0,
-        isDaily: false,
-        date: "",
-        month: "",
-        active: false
-    }
-
-
     this.Static.postResponse = {
         documentsInserted: 0,
         documentsModified: 0
